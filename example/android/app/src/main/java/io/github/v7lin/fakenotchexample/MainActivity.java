e581b5553fac18e0e0619fb1438516802ee13dc1
v7lin <v7lin@qq.com>
2019-05-16 12:00:07 +0800

    主题优化

--- example/android/app/src/main/java/io/github/v7lin/fakenotchexample/MainActivity.java
+++ example/android/app/src/main/java/io/github/v7lin/fakenotchexample/MainActivity.java
@@ -1,6 +1,10 @@
 package io.github.v7lin.fakenotchexample;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -9,6 +13,10 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+      getWindow().setStatusBarColor(Color.TRANSPARENT);
+      getWindow().setNavigationBarColor(Color.TRANSPARENT);
+    }
     GeneratedPluginRegistrant.registerWith(this);
   }
 }
